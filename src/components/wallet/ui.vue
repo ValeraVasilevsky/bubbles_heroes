50a7adbb97ed8368432c147c8dcebffc048a43ea
ValeraVasilevsky <valera200208@gmail.com>
2024-04-14 15:55:28 +0700

    fix: remove gh-pages

--- src/components/wallet/ui.vue
+++ src/components/wallet/ui.vue
@@ -14,7 +14,7 @@ const tonConnectUi = ref<TonConnectUI | null>(null);
 const initWallet = async (): Promise<void> => {
   tonConnectUi.value = new TonConnectUI({
     manifestUrl:
-      "https://valeravasilevsky.github.io/bubbles_heroes/tonconnect-manifest.json", //`https://eager-eagles-guess.loca.lt/wallet/manifest`,
+      "https://valeravasilevsky.github.io/bubbles_heroes/tonconnect-manifest.json",
     buttonRootId: "ton-connect",
   });
 };
